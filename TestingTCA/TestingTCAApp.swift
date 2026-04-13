60421e744341b9ab0997f86532c8a6bc9f82edde
gstfnsk <giulia.chimini@gmail.com>
2026-04-13 15:31:55 -0300

    Initial Commit

---
+++ TestingTCA/TestingTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTCAApp.swift
+//  TestingTCA
+//
+//  Created by Giulia Stefainski on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TestingTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
